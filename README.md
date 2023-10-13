d64b22c51871ec857d9d28f1001fa98ebed41a52
notshaangoswami <shaangoswami18@gmail.com>
2023-10-13 23:08:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock_Paper_Scissors_Game
+
+Hosted Link: https://kaverichougule.github.io/Rock_Paper_Scissors_Game/
---
+++ assets/script.js
@@ -0,0 +1,116 @@
+// First Page
+let audio = new Audio("./assets/audio.mp3");
+let firstSection = document.querySelector(".first_section");
+let playbtn = document.querySelector("#play");
+
+playbtn.addEventListener("click", () => {
+    audio.play();
+    let Main_container = document.querySelector(".container");
+    firstSection.style.display = "none";
+    Main_container.style.display = "block";
+});
+
+// Random Colors
+const container = document.querySelector(".container_box");
+let emoji = ["👊", "🖐️", "✂️"];
+for (let i = 0; i < 3; i++) {
+    let div1 = document.createElement("button");
+    div1.classList.add("btns");
+    container.appendChild(div1);
+}
+let color_div = document.querySelectorAll(".btns");
+color_div.forEach((single_color_div) => {
+    let color_code_generated = RandomColorGenerator();
+    single_color_div.style.backgroundColor = "#" + color_code_generated;
+});
+for (let i = 0; i < emoji.length; i++) {
+    color_div[i].innerText = emoji[i];
+    color_div[i].style.fontSize = "50px";
+}
+function RandomColorGenerator() {
+    let hexCode = "0123456789abcdef";
+    let CodeGenerated = "";
+    let lengthofColor = 6;
+    for (let i = 0; i < lengthofColor; i++) {
+        let randomIndex = Math.floor(Math.random() * 16);
+        CodeGenerated += hexCode[randomIndex];
+    }
+    return CodeGenerated;
+}
+
+// Game
+const resultMessage = document.querySelector(".result_message");
+const userScore_id = document.querySelector("#yourScore");
+const computerScore_id = document.querySelector("#computerScore");
+let Calculated_UserScore = 0;
+let Calculated_ComputerScore = 0;
+for (let i = 0; i < 3; i++) {
+    color_div[i].addEventListener("click", () => {
+        // console.log(color_div[i].innerText)
+        audio.play();
+        let result = MatchTheConditions(
+            color_div[i].innerText,
+            ComputerOutput()
+        );
+        resultMessage.innerText = result;
+        if (Calculated_UserScore === 5 || Calculated_ComputerScore === 5) {
+            resetToDefaults();
+        }
+    });
+}
+
+// Computer Value
+function ComputerOutput() {
+    let randomIndex = Math.floor(Math.random() * emoji.length);
+    // console.log(emoji[randomIndex])
+    return emoji[randomIndex];
+}
+
+function resetToDefaults() {
+    let won = "";
+    for (let i = 0; i < 3; i++) {
+        color_div[i].disabled = true;
+    }
+    won = Calculated_ComputerScore === 5 ? "Computer" : "You";
+    document.getElementById("final-result").innerText = `${won} won!`;
+    let replay_btn = document.querySelector(`button[replay="replay-btn"]`);
+    replay_btn.style.display = "block";
+    replay_btn.addEventListener("click", (e) => {
+        Calculated_ComputerScore = 0;
+        Calculated_UserScore = 0;
+        userScore_id.innerText = 0;
+        computerScore_id.innerText = 0;
+        resultMessage.innerText = "";
+        document.getElementById("final-result").innerText = "";
+        replay_btn.style.display = "none";
+        for (let i = 0; i < 3; i++) {
+            color_div[i].disabled = false;
+        }
+    });
+}
+
+// Check for the winner
+function MatchTheConditions(userValue, ComputerValue) {
+    if (userValue == ComputerValue) {
+        // resultMessage.innerText="It's a tie"
+        return "It's a tie";
+    } else if (
+        (userValue == "👊" && ComputerValue === "✂️") ||
+        (userValue == "✂️" && ComputerValue === "🖐️") ||
+        (userValue == "🖐️" && ComputerValue === "👊")
+    ) {
+        // resultMessage.innerText="You Won!! "+userValue+" beats "+ComputerValue
+        Calculated_UserScore++;
+        userScore_id.innerText = Calculated_UserScore;
+        return "You Won!! " + userValue + " beats " + ComputerValue;
+    } else {
+        // resultMessage.innerText="You Loose!! "+ComputerValue+" beats "+userValue
+        Calculated_ComputerScore++;
+        computerScore_id.innerText = Calculated_ComputerScore;
+        return "You Loose!! " + ComputerValue + " beats " + userValue;
+    }
+}
+
+// document.getElementById('my-score').addEventListener('', () => {
+//     console.log('Hello');
+// })
